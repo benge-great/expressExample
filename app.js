0ec126059aef85f9e5dc67ef8ea9e17eead5e74f
bge <bge@theknotww.com>
2020-04-25 21:55:26 +0800

    use error handler middleware

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const moment = require('moment');
 const cors = require('cors');
 const routers = require('./src/routers');
+const middlewares = require('./src/middlewares');
 const app = express();
 const port = 8080;
 app.use(cors({ 'Access-Control-Allow-Origin': '*' }));
 
 routers(app)
-
+middlewares(app)
 
 app.listen(port, () => console.log(`正在监听 ${port} 端口`))
