4ae6c2d0ac74799227244aa0e18eec9649dca5c6
bge <bge@theknotww.com>
2020-04-25 21:58:03 +0800

    update topic endpoint

--- src/routers/topic.js
+++ src/routers/topic.js
@@ -1,7 +1,8 @@
 const topicController = require('../controllers/topic')
+const asyncHandler = require('express-async-handler')
 
 module.exports = (app) => {
 
-  app.get('/topic/:slug', topicController.getTopic)
+  app.get('/topic/:slug', asyncHandler(topicController.getTopic))
 
 }
